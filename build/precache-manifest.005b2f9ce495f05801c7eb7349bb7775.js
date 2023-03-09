d780069d0bb02a040b3ac1866ae835efff234a34
Alex <alex.kylen@gmail.com>
2023-03-09 12:00:07 +0100

    init

---
+++ build/precache-manifest.005b2f9ce495f05801c7eb7349bb7775.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f09206023e065b4c4e968a3b0cb55da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4bbba550688c2a62e17",
+    "url": "/static/css/main.821be1c2.chunk.css"
+  },
+  {
+    "revision": "2dfbedba9f79cf50eb00",
+    "url": "/static/js/2.58f5a0db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58f5a0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4bbba550688c2a62e17",
+    "url": "/static/js/main.6f670815.chunk.js"
+  },
+  {
+    "revision": "72a81a76ac4aca142e54",
+    "url": "/static/js/runtime-main.9c0ec7fc.js"
+  },
+  {
+    "revision": "f1f6a656ab75337d6a6b63eda04d4186",
+    "url": "/static/media/logo.f1f6a656.png"
+  },
+  {
+    "revision": "2d133cb1ec0ed6e5d42852578d33fca7",
+    "url": "/static/media/wine.2d133cb1.svg"
+  }
+]);
