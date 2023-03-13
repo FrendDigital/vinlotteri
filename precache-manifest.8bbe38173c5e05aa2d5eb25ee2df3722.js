34375b32b0385da44f7cb4788ace2e4565dd5a5d
Alex <alex.kylen@gmail.com>
2023-03-13 15:47:57 +0100

    Updates

---
+++ precache-manifest.8bbe38173c5e05aa2d5eb25ee2df3722.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b13c6ae3610ed2a7507c5831013134",
+    "url": "/vinlotteri/index.html"
+  },
+  {
+    "revision": "d4bbba550688c2a62e17",
+    "url": "/vinlotteri/static/css/main.821be1c2.chunk.css"
+  },
+  {
+    "revision": "2dfbedba9f79cf50eb00",
+    "url": "/vinlotteri/static/js/2.58f5a0db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vinlotteri/static/js/2.58f5a0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4bbba550688c2a62e17",
+    "url": "/vinlotteri/static/js/main.6f670815.chunk.js"
+  },
+  {
+    "revision": "1d414fb909e005ce62b0",
+    "url": "/vinlotteri/static/js/runtime-main.cb3ce035.js"
+  },
+  {
+    "revision": "f1f6a656ab75337d6a6b63eda04d4186",
+    "url": "/vinlotteri/static/media/logo.f1f6a656.png"
+  },
+  {
+    "revision": "2d133cb1ec0ed6e5d42852578d33fca7",
+    "url": "/vinlotteri/static/media/wine.2d133cb1.svg"
+  }
+]);
